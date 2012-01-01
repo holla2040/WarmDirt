b01cad219cb76c4c26a9cfab8cb1bc56a6bb877d
Brightleaf Technologies <chollabaugh@rethinksun.com>
2012-01-01 14:39:38 -0700

    removing build dir

--- src/board/build-cli/WarmDirt.d
+++
@@ -1,9 +0,0 @@
-build-cli/WarmDirt.o: WarmDirt.cpp \
-  /opt/arduino/hardware/arduino/cores/arduino/WProgram.h \
-  /opt/arduino/hardware/arduino/cores/arduino/wiring.h \
-  /opt/arduino/hardware/arduino/cores/arduino/binary.h \
-  /opt/arduino/hardware/arduino/cores/arduino/WCharacter.h \
-  /opt/arduino/hardware/arduino/cores/arduino/WString.h \
-  /opt/arduino/hardware/arduino/cores/arduino/HardwareSerial.h \
-  /opt/arduino/hardware/arduino/cores/arduino/Stream.h \
-  /opt/arduino/hardware/arduino/cores/arduino/Print.h WarmDirt.h DHT.h
