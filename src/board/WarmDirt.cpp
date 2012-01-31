733045e0893dfdc0bf9e27df3f98007fd6dd25fb
Craig Hollabaugh <chollabaugh@rethinksun.com>
2012-01-31 00:33:38 -0700

    put a lower limit on pidoutput

--- src/board/WarmDirt.cpp
+++ src/board/WarmDirt.cpp
@@ -355,8 +355,8 @@ void WarmDirt::temperatureLoop() {
         //Serial.println(pidsetpoint);
 
         if (getLightSensor() < 500) {
-            if (pidoutput < 1500.0) {
-                pidoutput = 1500.0;
+            if (pidoutput < 1000.0) {
+                pidoutput = 1000.0;
             }
         }
 
